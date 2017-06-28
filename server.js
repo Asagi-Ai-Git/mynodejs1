a46c9bd0b329aecbe3262c8e9513dc4bf4e918bb
Asagi-Ai-Git <myrpextraemail-github@yahoo.com>
2017-06-28 12:44:16 -0400

    ...

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(process.env.TEXT);
-    res.end('Hello World\n');
-    
+    res.write("Hello from Github");
+    res.end();
 }).listen(port);
